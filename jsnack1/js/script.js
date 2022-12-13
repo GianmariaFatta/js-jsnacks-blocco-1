02138f9162254b7724018ff3fa7510497da0bfad
GianmariaFatta <gianmaria.fatta@gmail.com>
2022-12-13 20:30:15 +0100

    incomplete snack 1 commit

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -2,4 +2,21 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero. (qui potete usare un prompt). Il programma stampa la somma di tutti i numeri inseriti..
 
 
+const resultElement=document.getElementById("result");
+
+const somma = []
+
+for(i=1; i<=10; i++){
+    const userNumber = parseInt(prompt("dammi un numero"))
+
+    const sum = 0
+
+    sum+=userNumber[i]
+
+    console.log(userNumber)
+
+}
+
+
+
 
